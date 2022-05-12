606f2ec22f4ca240f48fe1a1f3a3c4e48ddc1dfc
Asif Alli <alli.asif@gmail.com>
2022-05-12 00:49:36 -0400

    package creation

--- src/main/java/com/example/alliconsulting/demo/controller/GreetingController.java
+++ src/main/java/com/example/alliconsulting/demo/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.alliconsulting.demo;
+package com.example.alliconsulting.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
